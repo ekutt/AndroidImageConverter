c4b32174f54d96edb8bfb724bacc4417c37d4e74
ekutt <emrekutt@gmail.com>
2020-08-05 19:18:33 +0300

    PNG to TIFF renaming

--- app/src/main/java/com/ekutt/example/imageconverter/ImageConverter.java
+++ app/src/main/java/com/ekutt/example/imageconverter/ImageConverter.java
@@ -1,14 +1,13 @@
 package com.ekutt.example.imageconverter;
 
 public class ImageConverter {
-  static {
-    System.loadLibrary("imageOps-jni");
-  }
-  
-  private static native void convertJPEGtoTIFF_Native(String filename, String foldername);
-
   public static void convertJPEGtoTIFF(String filename, String foldername)
   {
     ImageConverter.convertJPEGtoTIFF_Native(filename, foldername);
   }
+
+  static {
+    System.loadLibrary("imageOps-jni");
+  }
+  private static native void convertJPEGtoTIFF_Native(String filename, String foldername);
 }
